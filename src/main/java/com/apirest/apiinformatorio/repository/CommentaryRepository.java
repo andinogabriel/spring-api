59d5a1d7894bd463366f8c03a6238b2843a82585
Gabriel Andino <gabriel_andino@outlook.com>
2020-12-15 19:59:20 -0300

    Creo un repositorio para la clase comentario

--- src/main/java/com/apirest/apiinformatorio/repository/CommentaryRepository.java
+++ src/main/java/com/apirest/apiinformatorio/repository/CommentaryRepository.java
@@ -1,2 +1,8 @@
-package com.apirest.apiinformatorio.repository;public interface CommentaryRepository {
+package com.apirest.apiinformatorio.repository;
+
+import com.apirest.apiinformatorio.model.Commentary;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentaryRepository extends JpaRepository<Commentary, Long> {
+
 }
