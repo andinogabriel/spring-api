c0e442078443dce60b5f5ba0c1c0955e77a28b03
Gabriel Andino <gabriel_andino@outlook.com>
2020-12-15 17:21:00 -0300

    INF 0003 - Agrego querys al user para encontrar users de una determinada ciudad y users creados a partir de una fecha dada

--- src/main/java/com/apirest/apiinformatorio/service/UserService.java
+++ src/main/java/com/apirest/apiinformatorio/service/UserService.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Service;
 
 
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
