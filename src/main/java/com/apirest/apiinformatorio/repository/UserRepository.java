ba030788e8aa7d71057edfeff45450eb15771882
Gabriel Andino <gabriel_andino@outlook.com>
2020-12-15 11:52:59 -0300

    Agrego el Repositorio de usuario con la query custom para encontrar usuarios por ciudad pasada por parametro

--- src/main/java/com/apirest/apiinformatorio/repository/UserRepository.java
+++ src/main/java/com/apirest/apiinformatorio/repository/UserRepository.java
@@ -2,8 +2,16 @@ package com.apirest.apiinformatorio.repository;
 
 import com.apirest.apiinformatorio.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    //Consulta para obtener una lista de usuarios de una determinada ciudad pasada por parametro
+    @Query("select u from User u where u.city = :city")
+    public List<User> getUsersForCity(@Param("city") String city);
 
 
 }
