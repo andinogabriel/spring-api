bf57191275cc84b896a17819508716a03b5787e3
Gabriel Andino <gabriel_andino@outlook.com>
2020-12-15 17:23:14 -0300

    INF 0005 - Agrego querys para encontrar post que no hayan sido publicados y post que contengan una palabra determinada en el titulo

--- src/main/java/com/apirest/apiinformatorio/repository/PostRepository.java
+++ src/main/java/com/apirest/apiinformatorio/repository/PostRepository.java
@@ -2,6 +2,17 @@ package com.apirest.apiinformatorio.repository;
 
 import com.apirest.apiinformatorio.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query(value = "SELECT * FROM Post AS p WHERE p.title LIKE %:wordTitle%", nativeQuery = true)
+    public List<Post> findPostsByWordTitle(@Param("wordTitle") String wordTitle);
+
+    @Query("select p from Post p where p.published = 0")
+    public List<Post> getPostsNotPublished();
+
 }
