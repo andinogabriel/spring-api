47be84d2997823ed63d45e08672fe0631a325b4a
Gabriel Andino <gabriel_andino@outlook.com>
2020-12-18 17:48:31 -0300

    INF 0037 - Actualizo el repostorio de Post, cambiando las querys por funciones de busquedas nativas de JPA data

--- src/main/java/com/apirest/apiinformatorio/repository/PostRepository.java
+++ src/main/java/com/apirest/apiinformatorio/repository/PostRepository.java
@@ -2,17 +2,18 @@ package com.apirest.apiinformatorio.repository;
 
 import com.apirest.apiinformatorio.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query(value = "SELECT * FROM Post AS p WHERE p.title LIKE %:wordTitle%", nativeQuery = true)
-    public List<Post> findPostsByWordTitle(@Param("wordTitle") String wordTitle);
+    //SELECT * FROM Post AS p WHERE p.title LIKE %:wordTitle%
+    public List<Post> findByTitleContaining(String wordTitle);
 
-    @Query("select p from Post p where p.published = 0")
-    public List<Post> getPostsNotPublished();
+    //SELECT * FROM Post AS p WHERE p.published = 0
+    public List<Post> findByPublishedFalse();
+
+
 
 }
