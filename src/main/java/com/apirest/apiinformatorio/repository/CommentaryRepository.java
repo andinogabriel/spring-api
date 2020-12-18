4a42aea2a0c194f13673c2bc7a138627d26a0428
Gabriel Andino <gabriel_andino@outlook.com>
2020-12-18 17:50:03 -0300

    INF 0038 - Agrego una funcion nativa de JPA data para buscar todos los registros de un post determinado que luego se utiliza para obtener la longitud de la lista para mostrar el limite de comentarios a mostrar de un post determinado, porque sino pone el limite debera mostrar todos los comentarios de ese post, por eso esta funcion sirve para obtener la longitud de comentarios del post y pasarle como limite los N comentarios

--- src/main/java/com/apirest/apiinformatorio/repository/CommentaryRepository.java
+++ src/main/java/com/apirest/apiinformatorio/repository/CommentaryRepository.java
@@ -1,6 +1,7 @@
 package com.apirest.apiinformatorio.repository;
 
 import com.apirest.apiinformatorio.model.Commentary;
+import com.apirest.apiinformatorio.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,5 +12,9 @@ public interface CommentaryRepository extends JpaRepository<Commentary, Long> {
 
     @Query(value = "SELECT * FROM Commentary WHERE post_id=:id_post ORDER BY id DESC LIMIT :lim", nativeQuery = true)
     public List<Commentary> getCommentariesPostByLimit(@Param("id_post") Long id_post, @Param("lim") Integer lim);
+
+    public List<Commentary> findByPostOrderByIdDesc(Post post);
+
+
 
 }
