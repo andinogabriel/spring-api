f968073fcfa2b176dbb7d6591b0ac3e55726ca3b
Gabriel Andino <gabriel_andino@outlook.com>
2020-12-18 17:56:06 -0300

    INF 0047 - Actualizo el controller de City para normalizacion

--- src/main/java/com/apirest/apiinformatorio/controller/CityController.java
+++ src/main/java/com/apirest/apiinformatorio/controller/CityController.java
@@ -3,12 +3,13 @@ package com.apirest.apiinformatorio.controller;
 import com.apirest.apiinformatorio.model.City;
 import com.apirest.apiinformatorio.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("api/v1/city")
+@RequestMapping("api/v1/ciudad")
 @RestController
 public class CityController {
 
@@ -17,27 +18,28 @@ public class CityController {
 
     @GetMapping
     public ResponseEntity<List<City>> getCities() {
-        return cityService.getCities();
+        return ResponseEntity.ok().body(cityService.getCities());
     }
 
     @RequestMapping("{id}")
     public ResponseEntity<City> getCityById(@PathVariable("id") Long id) {
-        return cityService.getCityById(id);
+        return ResponseEntity.ok().body(cityService.getCityById(id));
     }
 
-    @PostMapping
+    @PostMapping("/agregar")
     public ResponseEntity<City> addCity(@RequestBody City city) {
-        return cityService.addCity(city);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cityService.addCity(city));
     }
 
-    @DeleteMapping(value = "{id}")
-    public ResponseEntity<String> deleteCityById (@PathVariable("id") Long id) {
-        return cityService.deleteCityById(id);
+    @DeleteMapping("/borrar/{id}")
+    public ResponseEntity<?> deleteCityById (@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(cityService.deleteCityById(id));
     }
 
-    @PutMapping
-    public ResponseEntity<City> updateCity (@RequestBody City city) {
-        return cityService.updateCity(city);
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<City> updateCity (@PathVariable("id") Long id, @RequestBody City city) {
+        city.setId(id);
+        return ResponseEntity.ok().body(cityService.updateCity(city));
     }
 
 }
