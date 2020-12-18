7c45e9531539746aef0a1786b0a2acafab3e9fe5
Gabriel Andino <gabriel_andino@outlook.com>
2020-12-18 17:55:44 -0300

    INF 0046 - Actualizo el controller de State para normalizacion

--- src/main/java/com/apirest/apiinformatorio/controller/StateController.java
+++ src/main/java/com/apirest/apiinformatorio/controller/StateController.java
@@ -4,12 +4,13 @@ import com.apirest.apiinformatorio.model.Country;
 import com.apirest.apiinformatorio.model.State;
 import com.apirest.apiinformatorio.service.StateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("api/v1/state")
+@RequestMapping("api/v1/provincia")
 @RestController
 public class StateController {
 
@@ -18,27 +19,28 @@ public class StateController {
 
     @GetMapping
     public ResponseEntity<List<State>> getStates() {
-        return stateService.getStates();
+        return ResponseEntity.ok().body(stateService.getStates());
     }
 
     @RequestMapping("{id}")
     public ResponseEntity<State> getStateById(@PathVariable("id") Long id) {
-        return stateService.getStateById(id);
+        return ResponseEntity.ok().body(stateService.getStateById(id));
     }
 
-    @PostMapping
+    @PostMapping("/agregar")
     public ResponseEntity<State> addState(@RequestBody State state) {
-        return stateService.addState(state);
+        return ResponseEntity.status(HttpStatus.CREATED).body(stateService.addState(state));
     }
 
-    @DeleteMapping(value = "{id}")
-    public ResponseEntity<String> deleteStateById (@PathVariable("id") Long id) {
-        return stateService.deleteStateById(id);
+    @DeleteMapping("/borrar/{id}")
+    public ResponseEntity<?> deleteStateById (@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(stateService.deleteStateById(id));
     }
 
-    @PutMapping
-    public ResponseEntity<State> updateState (@RequestBody State state) {
-        return stateService.updateState(state);
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<State> updateState (@PathVariable("id") Long id, @RequestBody State state) {
+        state.setId(id);
+        return ResponseEntity.ok().body(stateService.updateState(state));
     }
 
 }
