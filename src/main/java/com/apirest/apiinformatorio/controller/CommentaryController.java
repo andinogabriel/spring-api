545e38abb776f43e5e4695209f0f8b208f0212e8
Gabriel Andino <gabriel_andino@outlook.com>
2020-12-18 17:58:14 -0300

    INF 0050 - Actualizo el controller de Commentary

--- src/main/java/com/apirest/apiinformatorio/controller/CommentaryController.java
+++ src/main/java/com/apirest/apiinformatorio/controller/CommentaryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("api/v1/comentarios")
+@RequestMapping("api/v1/post/comentarios")
 @RestController
 public class CommentaryController {
 
@@ -27,31 +27,6 @@ public class CommentaryController {
     public ResponseEntity<Commentary> getCommentaryById(@PathVariable("id") Long id) {
         return ResponseEntity.ok().body(commentaryService.getCommentaryById(id));
     }
-
-    @PostMapping("{post_id}/comment")
-    public ResponseEntity<Commentary> addCommentary(@RequestBody Commentary commentary) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(commentaryService.addCommentary(commentary));
-    }
-
-    @PutMapping("{id}")
-    public ResponseEntity<Commentary> updateCommentary(@RequestBody Commentary commentary, @PathVariable("id") Long id) {
-        commentary.setId(id);
-        return ResponseEntity.ok().body(commentaryService.updateCommentary(commentary));
-    }
-
-
-    public HttpStatus deleteCommentaryById(@PathVariable("id") Long id) {
-        return HttpStatus.NO_CONTENT;
-    }
-
-    @GetMapping("/busqueda")
-    public ResponseEntity<List<Commentary>> getCommentariesPostByLimit(@RequestParam(value = "id_post") Long id_post, @RequestParam(value = "lim", required = false) Integer lim) {
-        if(lim == null) {
-            lim = commentaryService.getCommentaries().size();
-        }
-        return ResponseEntity.ok().body(commentaryService.getCommentariesPostByLimit(id_post, lim));
-    }
-
 
 
 }
