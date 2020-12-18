98bf711f2de5f7834828763b7125e729bb7874b3
Gabriel Andino <gabriel_andino@outlook.com>
2020-12-18 17:48:07 -0300

    INF 0036 - Actualizo el repostorio de User, cambiando las querys por funciones de busquedas nativas de JPA data

--- src/main/java/com/apirest/apiinformatorio/repository/UserRepository.java
+++ src/main/java/com/apirest/apiinformatorio/repository/UserRepository.java
@@ -1,21 +1,19 @@
 package com.apirest.apiinformatorio.repository;
 
+import com.apirest.apiinformatorio.model.City;
 import com.apirest.apiinformatorio.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    //Consulta para obtener una lista de usuarios de una determinada ciudad pasada por parametro
-    @Query("select u from User u where u.city = :city")
-    public List<User> getUsersForCity(@Param("city") String city);
 
-    @Query("select u from User u where u.registerDate > :registerDate")
-    public List<User> getUserForRegisterDate(@Param("registerDate") LocalDate date);
+    //SELECT * FROM User AS u WHERE u.city = :city
+    public List<User> findByCityEquals(City city);
 
+    //SELECT * FROM User AS u WHERE u.register_date > :register_date
+    public List<User> findByRegisterDateAfter(LocalDate date);
 
 }
