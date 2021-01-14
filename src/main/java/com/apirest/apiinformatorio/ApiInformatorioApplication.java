61cb6ec771b323d599cac1d8bc549ecd8722feb1
Gabriel Andino <gabriel_andino@outlook.com>
2021-01-14 14:13:21 -0300

    Actualizo el gitignore

---
+++ src/main/java/com/apirest/apiinformatorio/ApiInformatorioApplication.java
@@ -0,0 +1,14 @@
+package com.apirest.apiinformatorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInformatorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInformatorioApplication.class, args);
+	}
+}
+
+
